84ed656ff8cc6fab8a5de4ec136810477654643d
liamkimbley <39464176+liamkimbley@users.noreply.github.com>
2018-05-23 11:15:15 -0700

    Add files via upload

---
+++ PrintingChoicesEscapes.java
@@ -0,0 +1,8 @@
+public class PrintingChoicesEscapes
+{
+    public static void main( String[] args)
+    {
+        System.out.println("Alpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\n\nIndia\n\nThis is a test.\nI am learning Java the Hard Way!");
+
+    }
+}
