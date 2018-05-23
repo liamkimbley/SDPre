737ef7f755545b340600814d9a315e7c4d11edf1
liamkimbley <39464176+liamkimbley@users.noreply.github.com>
2018-05-23 10:15:42 -0700

    Add files via upload

---
+++ GasolineReceipt.java
@@ -0,0 +1,18 @@
+public class GasolineReceipt
+{
+    public static void main( String[] args)
+    {
+        System.out.println("+----------------------+");
+        System.out.println("|                      |");
+        System.out.println("|     CORNER STORE     |");
+        System.out.println("|                      |");
+        System.out.println("|  2015-05-23  10:06AM |");
+        System.out.println("|                      |");
+        System.out.println("|   Gallons: 10.870    |");
+        System.out.println("| Price/gallon: $2.089 |");
+        System.out.println("|                      |");
+        System.out.println("| Fuel Total:  $22.71  |");
+        System.out.println("|                      |");
+        System.out.println("+----------------------+");
+    }
+}
