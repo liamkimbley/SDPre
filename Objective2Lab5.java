716c64d7784ff8386eed0f2420bb31f1d5352d3b
liamkimbley <39464176+liamkimbley@users.noreply.github.com>
2018-05-25 10:15:56 -0700

    Add files via upload

---
+++ Objective2Lab5.java
@@ -0,0 +1,14 @@
+public class Objective2Lab5
+{
+    public static void main(String[] args)
+    {
+        double side1, side2, hypotenuse;
+
+        side1 = 5;
+        side2 = 8;
+        hypotenuse = Math.sqrt((side1*side1) + (side2*side2));
+
+        System.out.println(hypotenuse);
+
+    }
+}
