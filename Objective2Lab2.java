23378ab7e1d39aba26fcbfc7da72cddc4f296e80
liamkimbley <39464176+liamkimbley@users.noreply.github.com>
2018-05-25 09:19:48 -0700

    Add files via upload

---
+++ Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2
+{
+    public static void main(String[] args)
+    {
+        String name = "William Kimbley";
+        int age = 31;
+        String state = "California";
+
+        System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    }
+}
