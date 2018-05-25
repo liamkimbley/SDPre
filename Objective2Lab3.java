ff333b86897b2ce1aeda7c639e60588ca22f58ae
liamkimbley <39464176+liamkimbley@users.noreply.github.com>
2018-05-25 09:43:24 -0700

    Add files via upload

---
+++ Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3
+{
+    public static void main(String[] args)
+    {
+
+        String food1 = "In-N-Out";
+        String food2  = "Steak";
+        String food3 = "Apple";
+
+        System.out.println("My Favorite Foods Are: \n1. " + food1 + "\n2. " + food2 + "\n3. " + food3);
+    }
+}
