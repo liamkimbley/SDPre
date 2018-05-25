84e643910e0d12ec7584c2a05e3b50a2f4014661
liamkimbley <39464176+liamkimbley@users.noreply.github.com>
2018-05-25 08:37:20 -0700

    Add files via upload

---
+++ Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Name: William Kimbley");
+        System.out.println("Age: 31");
+        System.out.println("State: California");
+    }
+}
