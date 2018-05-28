42fe9621def1c17d606e552d8aaf898a10c79d2c
liamkimbley <39464176+liamkimbley@users.noreply.github.com>
2018-05-27 19:29:11 -0700

    Add files via upload

---
+++ Objective4Lab1.java
@@ -0,0 +1,16 @@
+public class Objective4Lab1
+{
+    public static void main(String[] args)
+    {
+        int num1 = 5, num2 = 15;
+
+        if (num1 > num2)
+        {
+          System.out.println(num1 + " is GREATER THAN " + num2);
+        }
+        else if (num1 < num2)
+        {
+          System.out.println(num2 + " is GREATER THAN " + num1);
+        }
+    }
+}
