68313f543e9b59958d70d578e739b32f42cb50fb
liamkimbley <39464176+liamkimbley@users.noreply.github.com>
2018-05-28 17:08:46 -0700

    Add files via upload

--- EnterPIN.java
+++ EnterPIN.java
@@ -6,11 +6,23 @@ public class EnterPIN
     {
         Scanner input = new Scanner(System.in);
         int pin, entry;
+        String password = "JavaTHW", attempt;
 
         pin = 12345;
 
         System.out.println("WELCOME TO THE BANK OF JAVA!");
-        System.out.print("ENTER YOUR PIN:\t");
+        System.out.print("PLEASE ENTER YOUR PASSWORD:\t");
+        attempt = input.next();
+
+        while (! attempt.equals(password))
+        {
+          System.out.println("\nINCORRECT PASSWORD. TRY AGAIN.");
+          System.out.print("ENTER YOUR PASSWORD:\t");
+          attempt = input.next();
+
+        }
+
+        System.out.print("THANK YOU. \nPLEASE ENTER YOUR PIN:\t");
         entry = input.nextInt();
 
         while (entry != pin)
