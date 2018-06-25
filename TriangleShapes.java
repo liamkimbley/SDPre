7511192dcb1790450c70fdb137b7c3487320f6d2
liamkimbley <39464176+liamkimbley@users.noreply.github.com>
2018-06-25 10:54:31 -0600

    Add files via upload

--- TriangleShapes.java
+++ TriangleShapes.java
@@ -5,9 +5,9 @@ public class TriangleShapes
   public static void main(String[] args)
   {
     Scanner input = new Scanner(System.in);
-    double a, b, c;
+    double a, b, c, d, e;
 
-    System.out.print("This will tell you what to classify your triangle.\n");
+    System.out.print("This will tell you what to classify your triangle as.\n");
     System.out.print("Please enter the length of each side of the triangle, one at a time.\n");
     System.out.print("What is the length of the first side? ");
     a = input.nextDouble();
@@ -18,22 +18,29 @@ public class TriangleShapes
     System.out.print("What is the length of the third side? ");
     c = input.nextDouble();
 
+    d = (a + b + c)/2;
+    e = Math.sqrt((d*(d-a)*(d-b)*(d-c)));
+
     if ((a == b) && (b == c))
     {
-      System.out.println("This triangle is an equilateral triangle.");
+      System.out.print("This triangle is an equilateral triangle.");
+      System.out.println("The area of this triangle is " + e);
     }
 
     else if ((a == b) || (a == c) || (b == c))
     {
       System.out.println("This triangle is an isosceles triangle.");
+      System.out.println("The area of this triangle is " + e);
     }
     else if ((c*c) == ((a*a)+(b*b)))
     {
       System.out.println("This triangle is a right triangle.");
+      System.out.println("The area of this triangle is " + e);
     }
     else
     {
       System.out.println("This triangle is a scalene triangle.");
+      System.out.println("The area of this triangle is " + e);
     }
   }
 }
